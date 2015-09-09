386398188cc08beac62e9cd8b8dbfe34fab4f5c3
cerebro500 <mr.bain@hotmail.com>
2015-09-08 23:16:08 -0500

    se agregan funciones multiplicacion y division

--- funciones.c
+++ funciones.c
@@ -5,5 +5,13 @@ float suma(float num1, float num2){
 }
 
 float resta(float num1, float num2){
-return num1 - num2;
+	return num1 - num2;
+}
+
+float multiplicacion(float num1, float num2){
+	return num1 * num2;
+}
+
+float division(float num1, float num2){
+	return num1/num2;
 }
