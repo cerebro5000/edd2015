11a35e73c07ceebecb6fda7c011c8d882ddd2386
cerebro500 <mr.bain@hotmail.com>
2015-09-08 22:56:40 -0500

    se agrega archivo funciones .h

---
+++ funciones.h
@@ -0,0 +1,18 @@
+#ifndef FUNCIONES
+
+#define FUNCIONES
+
+
+
+float suma(float num1, float num2);
+float resta(float num1, float num2);
+float multiplicacion(float num1, float num2);
+float division(float num1, float num2);
+
+
+
+
+
+
+
+#endif
