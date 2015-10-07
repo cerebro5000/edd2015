fd7f8b57410e10e67b0f252099813182440e9ff5
cerebro500 <mr.bain@hotmail.com>
2015-10-07 08:30:55 -0500

    se cambia forma de realizar operaciones de las matrices

--- matrices/matrices.c
+++ matrices/matrices.c
@@ -7,40 +7,53 @@ int main(){
 	printf("para la matriz 1 dame los sig datos");
 	for(i=0; i<3; i++){
 		for(j=0; j<3; j++){
-			printf("dame el dato de la fila %d de la columna %d\n",i+1,j+1);
-			scanf("%f",&arreglo1[i][j]);
+			printf("dame el dato de la fila %d de la columna %d\n",j+1,i+1);
+			scanf("%f",&arreglo1[j][i]);
 		}
 	}
 	printf("para la matriz 2 dame el sig dato ");
 	for(i=0; i<3; i++){
 		for(j=0; j<3; j++){
-			printf("dame el dato de la fila %d de la columna %d  \n ",i+1,j+1);
-			scanf("%f", &arreglo2[i][j]);
+			printf("dame el dato de la fila %d de la columna %d  \n ",j+1,i+1);
+			scanf("%f", &arreglo2[j][i]);
 		}
 	}
 	printf("las matrices que se multiplicaran son: \n");
+	printf("matriz 1");
 	for(i=0; i<3; i++){
 		printf("[");
 		for(j=0; j<3; j++){
-			printf("%f,",arreglo1[i][j]);
+			printf("%.2f,",arreglo1[j]i]);
 		}
 		printf("]\n");
 	}
+	printf("matriz 2");
 	for(i=0; i<3; i++){
-		printf("[");\
+		printf("[");
 		for(j=0; j<3; j++){
-			printf("%f,",arreglo2[i][j]);
+			printf("%.2f,",arreglo2[i][j]);
 		}
 		printf("]\n");
 	}
 
 	//comienzan las operaciones
-	for(i=0; i<3; i++){
-		for(j=0;j<3;j++){
-			acu = acu + arreglo1[i][j]*arreglo2[j][i];
-			mres[i][j]= acu;
-		}
+	for()
+	for(j=0;j<3;j++){
+		acu = acu + arreglo1[j][i] * arreglo2[i][j];
+		mres[j][i] = acu;
 	}
+	acu =0;
+	i=2;
+	for(j=0; j<3; j++){
+		acu = acu + arreglo1[j][i] * arreglo2[i][j];
+		mres[j][i] = acu;
+	}
+	acu=0;
+	i=3;
+	for(j=0; j<3; j++){
+		acu = acu + arreglo1[j][i] * arreglo2[i][j];
+	}
+	
 	//mostrar resultado de matrices
 	printf("el resultado es \n");
 	for(i=0; i<3; i++){
