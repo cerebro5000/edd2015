782d518069f11c7437a975f27616d5633e724399
cerebro500 <mr.bain@hotmail.com>
2015-10-10 23:15:30 -0500

    se agregan librerias unisth.h y cstdlib para uso de las funciones exit y usleep
    se agregan las siguentes funciones:
    pedirTamano
    multiplicacion
    validarOmatriz
    
    se llaman las siguentes funciones:
    pedirTamano
    validarOmatriz

--- matrices/nxnmatrices.cpp
+++ matrices/nxnmatrices.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <cstdlib>
+#include <unistd.h>
 using namespace std;
 
+void multiplicacion(float *a, float *b, float **c);
+void mostrarDatos(float **c, int *a, int *b);
+void validarOmatriz(int a, int b);
+void pedirTamanos(int *a, int *b);
 int main(){
-	float arreglo1[3][3], arreglo2[3][3], mres[3][3], acu=0;
-
-	cout<<"programa que multiplica matrices de 3x3"<<endl;
+	int tamx1, tamy1, tamx2,tamy2;
+	float arreglo1[tamx1][tamy1], arreglo2[tamx2][tamy2], mres[3][3], acu=0;
+	cout<<"programa que multiplica matrices de nxn"<<endl;
+	cout<<"agrega el tamano de la mtriz 1"<<endl;
+	pedirTamanos(&tamx1,&tamy1);
+	cout<<"agrega el tamano de la matriz 2"<<endl;
+	pedirTamanos(&tamx2,&tamy2);
+	validarOmatriz(tamx1, tamy2);
 	cout<<"para la matriz 1 dame los sig datos"<<endl;
+	
 	for(int i=0; i<3; i++){
 		for(int j=0; j<3; j++){
 			cout<<"dame el dato de la fila "<<j+1<<" de la columna"<<i+1<<endl;
@@ -46,14 +58,35 @@ int main(){
 			acu =0;
 		}
 	}
-	//se muestra el resultado
+	return 0;
+}
+void pedirTamanos(int *a, int *b){
+	
+	cout<<"dame el tamano de las filas"<<endl;
+	cin>>*a;
+	cout<<"dame el unmero de las columnas"<<endl;
+	cin>>*b;
+}
+
+void  multiplicacion(float *a, float *b, float **c){
+}
+void validarOmatriz(int a, int b){
+	if(a == b){
+		cout<<"datos correctos se pueden realizar"<<endl;
+	}
+	else{
+		cout<<"datos erroneos cerrando programa"<<endl;
+		usleep(4000);
+		exit(EXIT_FAILURE);
+	}
+}
+void mostrarDatos(float **c, int *a, int *b){
 	cout<<"el resultado es"<<endl;
-	for(int i=0; i<3; i++){
+	for(int i = 0 ; i < *b ; i++){
 		cout<<"[";
-		for(int j=0 ; j<3; j++){
-			cout<<mres[j][i]<<",";
+		for(int j = 0 ; j < *a ; j++){
+			cout<<*c[j][i]<<", ";
 		}
 		cout<<"]"<<endl;
 	}
-	return 0;
 }
