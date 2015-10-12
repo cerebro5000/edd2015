52f02f7c64d73b17dcaf26539cbf7135598763b5
cerebro500 <mr.bain@hotmail.com>
2015-10-12 02:04:52 -0500

    se cambian nombres de variables y se corrigen errores de ejecucion

--- matrices/nxnmatrices.cpp
+++ matrices/nxnmatrices.cpp
@@ -13,51 +13,52 @@ void multiplicacion(float **, float**, float **, int, int);
 
 
 int main(){
-	int tx1,ty1,tx2,ty2;
+	int c1,f1,c2,f2;
 	float **m1,**m2,**mres;
 
 	cout<<"programa que multiplica matrices de nxn"<<endl;
-	//seingresan los tamanos de las matrices
-	cout<<"dame los tamanos de la matriz 1"<<endl;
-	ingresarTamano(&tx1,&ty1);
+	//se ingresan los tamanos de las matrices
+	cout<<"dame el tamano de la matriz 1"<<endl;
+	ingresarTamano(&c1,&f1);
 	cout<<"dame el tamano de la matriz 2 "<<endl;
-	ingresarTamano(&tx2,&ty2);
+	ingresarTamano(&c2,&f2);
 
 	//validamos si la operacion se peude realizar
-	validarOperacion(&ty1,&tx2);
+	validarOperacion(&c1,&f2);
 
 	//creamos las matrices
-	m1 = crearM(&tx1, &ty1);
-	m2 = crearM(&tx2, &ty2);
-	mres = crearM(&ty1, &tx2);
+	m1 = crearM(&f1, &c1);
+	m2 = crearM(&f2, &c2);
+
+	mres = crearM(&f1, &c2);
 
 	//se igresa datos a las matrices
 	cout<<"ingresa los datos de las matrices:"<<endl;
 	cout<<"matriz 1"<<endl;
-	ingresaDatos(m1,tx1,ty1);
+	ingresaDatos(m1,f1,c1);
 	cout<<"matriz 2"<<endl;
-	ingresaDatos(m2,tx2,ty2);
+	ingresaDatos(m2,f2,c2);
 
 	//se imprimen las matrices
 	cout<<"matriz 1"<<endl;
-	imprimeM(m1, tx1, ty1);
+	imprimeM(m1, f1, c1);
 	cout<<"matriz 2"<<endl;
-	imprimeM(m2, tx2, ty2);
+	imprimeM(m2, f2, c2);
 
 	//se ejecutan las operaciones
-	multiplicacion(m1, m2, mres, tx1, ty1);
+	multiplicacion(m1, m2, mres, c2, f1);
 	//se muestra el resultado
 	cout<<"el resultado es:"<<endl;
-	imprimeM(mres,tx1,ty2);
+	imprimeM(mres,f1,c2);
 
 	//liberamos la memoria
-	for(int i=0; i<tx1; i++)
+	for(int i=0; i<f1; i++)
 		delete [] m1[i];
 	delete m1;
-	for(int i=0; i<tx2; i++)
+	for(int i=0; i<f2; i++)
 		delete [] m2[i];
 	delete m2;
-	for(int i=0; i<tx1; i++)
+	for(int i=0; i<f1; i++)
 		delete [] mres[i];
 	delete mres;
 
@@ -73,19 +74,19 @@ void validarOperacion(int *x, int *y){
 	}
 }
 void ingresarTamano(int *x, int *y){
-	cout<<"ingresa el numero de filas"<<endl;
+	cout<<"ingresa el numero de columnas"<<endl;
 	cin>>*x;
-	cout<<"ingrersa el numero de columnas"<<endl;
+	cout<<"ingrersa el numero de filas"<<endl;
 	cin>>*y;
 }
-float** crearM(int *x,int *y){
-	float **m = new float*[*y];
-	for(int i = 0; i < *x; i++){
-		m[i]= new float[*x];
+float** crearM(int *y,int *x){
+	float **m = new float*[*x];
+	for(int i = 0; i < *y; i++){
+		m[i]= new float[*y];
 	}
 	return m;
 }
-void ingresaDatos(float**matriz, int x, int y){
+void ingresaDatos(float**matriz, int y, int x){
 	for(int j =0 ; j < y ; j++){
 		for(int i =0 ; i < x ; i++){
 			cout<<"dame el dato para la columna "<<i+1<<"fila "<<j+1<<endl;
@@ -93,7 +94,7 @@ void ingresaDatos(float**matriz, int x, int y){
 		}
 	}
 }
-void imprimeM(float **matriz, int x, int y){
+void imprimeM(float **matriz, int y, int x){
 	for(int i=0; i<y; i++){
 		cout<<"[";
 		for(int j=0; j<x; j++){
@@ -102,15 +103,15 @@ void imprimeM(float **matriz, int x, int y){
 		cout<<"]"<<endl;
 	}
 }
-void multiplicacion(float **m1, float**m2, float **res, int x, int y){
-	float acu =0;
+void multiplicacion(float **m1, float**m2, float **res, int y, int x){
+	float acu = 0;
 	for(int k=0; k<y; k++){
 		for(int i=0; i<x; i++){
 			for(int j=0; j<x; j++){
-				acu = acu + m1[j][k] * m2[i][j];
+				acu = acu + m1[j][i] * m2[k][j];
 			}
-			res[i][k]=acu;
-			acu =0;
+			res[k][i]=acu;
+			acu=0;
 		}
 	}
 }
