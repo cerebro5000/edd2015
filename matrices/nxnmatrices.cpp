e7837831ec7441c52d62445ab02c62bcaa66667a
cerebro500 <mr.bain@hotmail.com>
2015-10-11 20:59:55 -0500

    se cambia el metodo crearM por ingresarDatos

--- matrices/nxnmatrices.cpp
+++ matrices/nxnmatrices.cpp
@@ -5,16 +5,17 @@ using namespace std;
 
 void validarOperacion(int *, int *);
 void ingresarTamano(int *, int *);
-void crearM(float**,int*,int*);
+void crearM(float**, int*, int*);
+void ingresaDatos(float**, int, int);
 int main(){
 	int tx1,ty1,tx2,ty2;
 	float **m1,**m2,**mres;
 	float arreglo1[3][3], arreglo2[3][3], acu=0;
 	cout<<"programa que multiplica matrices de nxn"<<endl;
 	//seingresan los tamanos de las matrices
-	cout<<"para la matriz 1 dame los sig datos"<<endl;
+	cout<<"dame los tamanos de la matriz 1"<<endl;
 	ingresarTamano(&tx1,&ty1);
-	cout<<"para la matriz 2 dame el sig dato "<<endl;
+	cout<<"dame el tamano de la matriz 2 "<<endl;
 	ingresarTamano(&tx2,&ty2);
 
 	//validamos si la operacion se peude realizar
@@ -25,24 +26,16 @@ int main(){
 	crearM(m2,&tx2,&ty2);
 	crearM(mres,&tx1&ty2);
 
-	//se imprimen las matrices
-	cout<<"las matrices que se multiplicaran son:"<<endl;
+	//se igresa datos a las matrices
+	cout<<"ingresa los datos de las matrices:"<<endl;
 	cout<<"matriz 1"<<endl;
-	for(int i=0; i<3; i++){
-		cout<<"[";
-		for(int j=0; j<3; j++){
-			cout<<arreglo1[j][i]<<", ";
-		}
-		cout<<"]"<<endl;
-	}
+	ingresaDatos(m1,tx1,ty1);
 	cout<<"matriz 2"<<endl;
-	for(int i=0; i<3; i++){
-		cout<<"[";
-		for(int j=0; j<3; j++){
-			cout<<arreglo2[j][i]<<", ";
-		}
-		cout<<"]"<<endl;
-	}
+	ingresaDatos(m2,tx2,ty2);
+
+	//se imprimen las matrices
+
+
 	//se ejecutan las operaciones
 	for(int k=0; k<3; k++){
 		for(int i=0; i<3; i++){
@@ -83,5 +76,13 @@ void crearM(float **m,int *x,int *y){
 	m = new iny*[*y];
 	for(int i = 0; i < *x; i++){
 		m[i]= new *[*x];
+	}
+}
+void ingresaDatos(float**matriz,int x, int y){
+	for(int j =0 ; j < y ; j++){
+		for(int i =0 ; i < x ; i++){
+			cout<<"dame el dato para la columna "<<x<<"fila "<<y<<endl;
+			cin>>matriz[i][j];
+		}
 	}
 }
