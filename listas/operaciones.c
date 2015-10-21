75e49703f94163ea0a71e5b268e109d09507888b
cerebro500 <mr.bain@hotmail.com>
2015-10-20 22:41:50 -0500

    se arreglan errores de ejecucion y se termina metodo imprimir lista

--- listas/operaciones.c
+++ listas/operaciones.c
@@ -1,4 +1,7 @@
 #include "operaciones.h"
+#include "nodo.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void inicializar(struct nodo *cabecera){
 	cabecera->sig = NULL;
@@ -7,48 +10,48 @@ void insertar_al_principio(struct nodo *cabecera, int valor_nuevo){
 	struct nodo *nuevo;
 	nuevo = (struct nodo*) malloc(sizeof(struct nodo) * 1);
 	nuevo-> val = valor_nuevo;
-		
 	nuevo->sig = cabecera->sig;
 	cabecera->sig = nuevo;
 }
-void instertar_al_final(struct nodo *cabecera, int valor_nuevo, int valor_antes){
-	struct nodo *actual;
+void insertar_al_final(struct nodo *cabecera, int valor_nuevo){
 	struct nodo *nuevo;
+	struct nodo *actual;
+	actual = cabecera;
+	nuevo = (struct nodo*) malloc(sizeof(struct nodo) *1);
 	nuevo->val = valor_nuevo;
-	actual->sig = cabecera->sig;
 	while(actual->sig != NULL){
-		actual->sig = actual->sig;
-		
+		actual =actual->sig;
 	}
 	nuevo->sig = actual->sig;
-	actual->sig = nuevo;
+	actual->sig= nuevo;
 }
 void insertar_despues_de(struct nodo *cabecera, int valor_nuevo, int valor_antes){
 	struct nodo *nuevo;
 	struct nodo *actual;
+	nuevo = (struct nodo*) malloc(sizeof(struct nodo)*1);
 	nuevo->val = valor_nuevo;
-	actual = cabecera->sig;
-	if(cabecera->sig != NULL){
-		
-		while(true){
-			actual = actual->sig;
-			if(actual->val == valor_antes){
-				nuevo->sig = actual->sig;
-				actual->sig = nuevo;
-				break;
-			}
+	actual = cabecera;
+	
+	while(actual->sig != NULL){
+		actual = actual->sig;
+		if(actual->val == valor_antes){
+			nuevo->sig = actual->sig;
+			actual->sig = nuevo;
+			break;
 		}
 	}
 }
 void imprimir_lista(struct nodo * cabecera){
 	struct nodo *actual;
-	actual->sig = cabecera->sig;
+	actual = cabecera;
 	while(actual->sig != NULL){
-		cout<<*cabecera.sig<<","<<endl;
+		actual = actual->sig;
+		printf("%d, ",actual->val);
 	}
+	printf("\n");
 }
 
-void borrar_lista(struct nodo cabecera){
+void borrar_lista(struct nodo *cabecera){
 	struct nodo *actual;
 	struct nodo *borrar;
 
@@ -66,5 +69,3 @@ void borrar_lista(struct nodo cabecera){
 		cabecera->sig = NULL;
 	}
 }
-
-
--- listas/programa1.c
+++ listas/programa1.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include "nodo.h"
 #include "operaciones.h"
+#include <stdlib.h>
 
 
 int main (){
 	struct nodo *cabecera;
 	int valor;
-	printf(" si mandas -1 veras la lista y saldras el programa\n"); 
-	scanf("%d", &valor);
+	cabecera = (struct nodo*) malloc(sizeof(struct nodo));
 	inicializar(cabecera);
-	while (valor != -1){
+	printf(" si mandas -1 veras la lista y saldras el programa\n");
+	while(1){
+		printf("dame un numero: ");
+		scanf("%d",&valor);
+		if(valor==-1)
+			break;
 		insertar_al_final(cabecera, valor);
 	}
 	imprimir_lista(cabecera);
-	
+	borrar_lista(cabecera);
 	return 0;
 }
