6d82571fc1230e7899d03131f2eaa17d2fa1d40a
cerebro500 <mr.bain@hotmail.com>
2015-10-21 06:55:03 -0500

    se agrega impresion de la lista

--- listas/programa3.c
+++ listas/programa3.c
@@ -22,6 +22,7 @@ int main(){
 	printf("dime que dato quieres agregar despues de %d",despues);
 	scanf("%d",&valor);
 	insertar_despues_de(cabecera,valor,despues);
+	imprimir_lista(cabecera);
 	borrar_lista(cabecera);
 	return 0;
 	
