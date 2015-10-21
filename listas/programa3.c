f40ada71799102e3cab06d007e21f044106996ff
cerebro500 <mr.bain@hotmail.com>
2015-10-21 06:43:45 -0500

    se agrega el 3er programa

---
+++ listas/programa3.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "nodo.h"
+#include "operaciones.h"
+
+void main(){
+	struct nodo *cabecera;
+	int valor,despues;
+	cabecera = (struct nodo*) malloc(sizeof(struct nodo));
+
+	printf("agrega datos y con -1 para ya no agregar mas\n");
+	while(1){
+		printf("dame un numero: ");
+		scanf("%d",&valor);
+		if(valor == -1)
+			break;
+		insertar_al_principio(cabecera,valor);
+	}
+	imprime_lista(cabecera);
+	printf("despues de que numero quieres agregar algo");
+	scanf("%d",&despues);
+	printf("dime que dato quieres agregar despues de %d",despues);
+	scanf("%d",&valor);
+	insertar_despues_de(cabecera,valor,despues);
+}
