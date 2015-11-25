28b9d58b93621bf6e4ffe52226520d6609f2fc47
cerebro500 <mr.bain@hotmail.com>
2015-11-25 15:33:23 -0600

    se corrigen errrores de compilacion

--- arboles/main.c
+++ arboles/main.c
@@ -9,35 +9,40 @@ void ingresar_datos(char *val){
 	printf("dame todos los valores  que quieres agregar");
 	scanf("%s",val);
 }
-void insertar_en_lista(struct nodo * cola, char *val, int tam, struct nodo *pila){
+void insertar_en_lista(struct nodo * cola, char *val, int tam){
 	int i;
-	char a;
 	for(i = 0 ; i < tam ; i++){
 		insertar_al_final(cola,val[i]);
 	}
 }
 
 void post_fijo(struct nodo * pila, struct nodo * cola, struct nodo *post){
-	int prio=0;
+	int prio=0,i;
 	struct nodo *actual;
 	actual = cola;
 	while(actual->sig != NULL){
 		actual= actual->sig;
 		if(actual->val == '+' || actual->val == '-'){
-			if(prio == 1 && tope(pila) == '+'){
+			if(prio == 1 && (tope(pila) == '+'|| tope(pila) == '-')){
 				insertar_al_final(post,tope(pila));
 				pop(pila);
+				push(pila,actual->val);
 			}
 			else
 				push(pila,actual->val);
+			if(prio == 2 && (tope(pila) == '*' || tope(pila) == '/')){
+				insertar_al_final(post,tope(pila));
+				pop(pila);
+				insertar_al_final(post,tope(pila));
+				pop(pila);
+			}
 			prio = 1;
 		}
 		if(actual->val == '*' || actual->val == '/'){
-			if(prio == 2 && tope(pila) == '*'){
+			if(prio == 2 && (tope(pila) == '*' || tope(pila) == '/')){
 				insertar_al_final(post,tope(pila));
 				pop(pila);
-				insertar_al_final(post,tope(pila));
-				pop(pila);
+				push(pila,actual->val);
 			}
 			else
 				push(pila,actual->val);
@@ -47,22 +52,32 @@ void post_fijo(struct nodo * pila, struct nodo * cola, struct nodo *post){
 			insertar_al_final(post,actual->val);
 		}
 	}
+	printf("%d\n\n",prio);
+	for(i = 0 ; i < prio; i++){
+		insertar_al_final(post,tope(pila));
+		pop(pila);
+	}
 }
 int main(){
-	int tam,n;
-	char valores[n];
+	int tam;
+	char *valores;
 	struct nodo *pila, *lista, *post;
 	printf("dime cuantos valores ingresaras\n");
 	scanf("%d",&tam);
-	n = tam + (tam-1);
+	tam = tam + (tam -1);
+	valores = malloc(sizeof(char) * tam );
 	pila = inicializar();
 	lista = inicializar();
 	post = inicializar();
 	ingresar_datos(valores);
-	insertar_en_lista(lista,valores,n,pila);
+	insertar_en_lista(lista,valores,tam);
 	post_fijo(pila,lista,post);
+	printf("infijo: ");
 	imprimir_lista(lista);
+	printf("postfijo: ");
+	imprimir_lista(post);
 	free(pila);
 	free(lista);
 	free(post);
+	free(valores);
 }
