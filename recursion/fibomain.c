76b710257581de1123a5ef22aeeb18a9df7654a6
cerebro500 <mr.bain@hotmail.com>
2015-12-14 08:35:54 -0600

    se arreglan errores de ejecucion

--- recursion/fibomain.c
+++ recursion/fibomain.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void fibo(int num, int max, res){
+void fibo(int num, int max, int res){
 	res = res + num;
-	pritf("%d",res);
+	printf("%d, ",res);
 	if(res < max)
-		fibo(res,max,res)
+		fibo(res,max,num);
 }
 int main(){
-	int numero=0, ulti, res=1;
+	int numero=1, ulti, res = 0;
 	printf("programa de fibonacci recursivo\n");
-	pritnf("dime maximo hasta que numero quieres ver");
+	printf("dime maximo hasta que numero quieres ver");
 	scanf("%d",&ulti);
-	printf("la sucecion es :%d", numero);
+	printf("la sucecion es :%d, %d, ", res, numero);
 	fibo(numero, ulti, res);
 	printf("\n");
 }/*created by: cerebro*/
